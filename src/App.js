c260acab59abc88e3ee900bc37920f73a1f21df3
Levss30 <lucas.levasseur18@gmail.com>
2023-07-07 03:54:13 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Paragraph from "./components/Paragraph";
+import Title from "./components/Title";
+import "./styles/App.css"
 
 function App() {
+
+  const [step, setStep] = useState(1)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <aside>
+          <div className="steps">
+            <div className={`step ${step === 1 && "active"}`}>
+              <div className="step-number">
+                1
+              </div>
+              <div className="step-info">
+                <Paragraph>STEP 1</Paragraph>
+                <Title>Suas informações</Title>
+              </div>
+
+            </div>
+            <div className="step">
+              <div className="step-number">
+                2
+              </div>
+              <div className="step-info">
+                <Paragraph>STEP 2</Paragraph>
+                <Title>Selecione o plano</Title>
+              </div>
+
+            </div>
+            <div className="step">
+              <div className="step-number">
+                3
+              </div>
+              <div className="step-info">
+                <Paragraph>STEP 3</Paragraph>
+                <Title>Suas informações</Title>
+              </div>
+            </div>
+            <div className="step">
+              <div className="step-number">
+                4
+              </div>
+              <div className="step-info">
+                <Paragraph>STEP 4</Paragraph>
+                <Title>Sumário</Title>
+              </div>
+            </div>
+          </div>
+        </aside>
+        <div className="content">
+          <button onClick={() => setStep(2)}>Mudar step pra 2</button>
+          {step === 1 && (
+            <form>
+              <Title className="form-title">Informações pessoais</Title>
+              <Paragraph className="form-description">Forneça suas informações para o cadastro</Paragraph>
+
+            </form>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
